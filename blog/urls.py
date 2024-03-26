f0d7517384cf8ed6ba29819da7a2226c1fda55f6
Mainul Islam Mahi <18-38468-2@student.aiub.edu>
2024-03-26 20:25:18 +0600

    3rd

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +24,4 @@ urlpatterns = [
     path('about/', views.About),
     path('', views.Home),
 ]
+urlpatterns += staticfiles_urlpatterns()
