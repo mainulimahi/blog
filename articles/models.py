eb3409a77146b310f1244bcb80f0ae61ef7da5a4
Mainul Islam Mahi <18-38468-2@student.aiub.edu>
2024-03-27 20:27:11 +0600

    4th

--- articles/models.py
+++ articles/models.py
@@ -7,6 +7,8 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
+    #date = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.title
     
