810fa603a58d494ca9d5a827a52d5de02ed6f7df
Mainul Islam Mahi <18-38468-2@student.aiub.edu>
2024-03-27 20:30:00 +0600

    4th

--- articles/urls.py
+++ articles/urls.py
@@ -5,9 +5,3 @@ from . import views
 urlpatterns = [
     path('', views.article_list),
 ]
-
-#date = models.DateTimeField(auto_now_add=True)
-    #date = models.DateTimeField(auto_now_add=True)
-    #date = models.DateTimeField(auto_now_add=True)
-    #date = models.DateTimeField(auto_now_add=True)
-    #date = models.DateTimeField(auto_now_add=True)
